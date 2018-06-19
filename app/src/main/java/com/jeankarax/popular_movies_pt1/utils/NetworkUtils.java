da05fe30222daae004174cf4329c99a5072a958c
Jean Karax <jeankarax@Jeans-MacBook-Air.local>
2018-06-19 17:29:16 -0300

    started networking

---
+++ app/src/main/java/com/jeankarax/popular_movies_pt1/utils/NetworkUtils.java
@@ -0,0 +1,11 @@
+package com.jeankarax.popular_movies_pt1.utils;
+
+/**
+ * Created by Jean Karax.
+ */
+
+public class NetworkUtils {
+
+    final static String THE_MOVIEDB_BASE_URL = "http://api.themoviedb.org/3/";
+
+}
