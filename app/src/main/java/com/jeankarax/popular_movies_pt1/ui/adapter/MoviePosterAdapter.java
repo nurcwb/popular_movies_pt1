dd58ef36bac7e07c80f621fe0f338ed7a9c80047
Jean Karax <jeankarax@Jeans-MacBook-Air.local>
2018-06-19 14:20:58 -0300

    Finished postar adapter

--- app/src/main/java/com/jeankarax/popular_movies_pt1/ui/adapter/MoviePosterAdapter.java
+++ app/src/main/java/com/jeankarax/popular_movies_pt1/ui/adapter/MoviePosterAdapter.java
@@ -1,6 +1,7 @@
 package com.jeankarax.popular_movies_pt1.ui.adapter;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.ImageView;
 
 import com.jeankarax.popular_movies_pt1.R;
 import com.jeankarax.popular_movies_pt1.model.MovieData;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -16,15 +18,19 @@ public class MoviePosterAdapter extends RecyclerView.Adapter<MoviePosterAdapter.
 
     private List<MovieData> mMovieList;
 
-    public MoviePosterAdapter(List<MovieData> movieList){
+    private String mImageUrl = "http://image.tmdb.org/t/p/w185/";
+    private Context mCtx;
+
+    public MoviePosterAdapter(Context ctx, List<MovieData> movieList){
+        mCtx = ctx;
         mMovieList = movieList;
     }
 
     @Override
     public PosterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context ctx = parent.getContext();
+
         int layoutIdForGridItem = R.layout.activity_movie_poster_item;
-        LayoutInflater inflater = LayoutInflater.from(ctx);
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
 
         View view = inflater.inflate(layoutIdForGridItem, parent, false);
         PosterViewHolder posterViewHolder = new PosterViewHolder(view);
@@ -34,7 +40,9 @@ public class MoviePosterAdapter extends RecyclerView.Adapter<MoviePosterAdapter.
 
     @Override
     public void onBindViewHolder(PosterViewHolder holder, int position) {
-
+        Uri movieUri = Uri.parse(mImageUrl).buildUpon().appendEncodedPath(mMovieList.get(position)
+                .getPoster_path()).build();
+        Picasso.with(mCtx).load(movieUri).into(holder.ivMoviePoster);
     }
 
     @Override
